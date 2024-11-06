72cf919db8c841093739e15a77378f2d1b773530
marvinlanhenke <lanhenke@googlemail.com>
2024-11-06 15:52:35 +0100

    feat: add airflow airbyte operator

--- airflow/dags/example/main_dag.py
+++ airflow/dags/example/main_dag.py
@@ -1,9 +1,12 @@
 from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from pendulum import datetime
 
 default_args = {"owner": "marvinlanhenke"}
 
+
+CONNECTION_ID = "276d2442-4cda-4353-abe1-2d6e048955dd"
 
 @dag(
     default_args=default_args,
@@ -14,9 +17,16 @@ default_args = {"owner": "marvinlanhenke"}
 )
 def dwh_airbyte_dbt_example() -> None:
     start = EmptyOperator(task_id="start")
+
+    extract = AirbyteTriggerSyncOperator(
+        task_id="airbyte_extract_example",
+        airbyte_conn_id="airbyte_conn",
+        connection_id=CONNECTION_ID,
+    )
+
     end = EmptyOperator(task_id="end")
 
-    start >> end  # type: ignore
+    start >> extract >> end  # type: ignore
 
 
 dwh_airbyte_dbt_example()
